6ddfe2df099cb91f4771c1f5a5ffb5e6a18bfe15
Adagio857 <hudson.miyake@gmail.com>
2026-05-17 20:15:03 -0400

    added users

--- src/app-users.ts
+++ src/app-users.ts
@@ -1,3 +1,18 @@
 export const APP_USERS = [
-  { username: "HudsonM", password: "1648" }
+  { username: "DutchC", password: "1648" },
+  { username: "EleanorS", password: "1648" },
+  { username: "EricK", password: "1648" },
+  { username: "ErikB", password: "1648" },
+  { username: "EsanE", password: "1648" },
+  { username: "GrayJ", password: "1648" },
+  { username: "HudsonM", password: "1648" },
+  { username: "LelaM", password: "1648" },
+  { username: "MiloP", password: "1648" },
+  { username: "MollyT", password: "1648" },
+  { username: "NickH", password: "1648" },
+  { username: "NoahT", password: "1648" },
+  { username: "PeterB", password: "1648" },
+  { username: "RussellM", password: "1648" },
+  { username: "WenP", password: "1648" },
+  { username: "WoodwardS", password: "1648" }
 ] as const;
